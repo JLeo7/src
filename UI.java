938de3cc0a5cb1938eb0aeeeea473a9793128ef8
JLeo7 <jleomoram@gmail.com>
2018-04-14 20:36:54 -0600

    UI

---
+++ UI.java
@@ -0,0 +1,168 @@
+import com.softwareCelestial.*; //Quitar esto cuando se importe la libreria.
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+public class UI {
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static PrintStream out = System.out;
+
+    public static void main(String[] args) throws java.io.IOException {
+        int option;
+        boolean mainExit = false;
+
+        do {
+            showMainMenu();
+            option = readOption();
+            mainExit = execMainAction(option);
+        } while (!mainExit);
+    }
+
+    static void showMainMenu() {
+        out.println();
+        out.println("Menu");
+        out.println();
+        out.println("1.  Option 1.");
+        out.println("2.  Option 2.");
+        out.println("0.  Exit.");
+        out.println();
+    }
+
+    static void showOpt1Menu() {
+        out.println();
+        out.println("Option 1");
+        out.println();
+        out.println("1.  .");
+        out.println("2.  .");
+        out.println("3.  .");
+        out.println("4.  .");
+        out.println("0.  Go back.");
+        out.println();
+    }
+
+    static void showOpt2Menu() {
+        out.println();
+        out.println("Option 2");
+        out.println();
+        out.println("1.  .");
+        out.println("2.  .");
+        out.println("3.  .");
+        out.println("4.  .");
+        out.println("0.  Go back.");
+        out.println();
+    }
+
+    static void showOpt3Menu() {
+        out.println();
+        out.println("Option 3");
+        out.println();
+        out.println("1.  .");
+        out.println("2.  .");
+        out.println("3.  .");
+        out.println("4.  .");
+        out.println("0.  Go back.");
+        out.println();
+    }
+
+    static int readOption() throws java.io.IOException {
+        int option;
+        out.print("Select your option: ");
+        option = Integer.parseInt(in.readLine());
+        out.println();
+        return option;
+    }
+
+    static boolean execMainAction(int poption) throws java.io.IOException {
+        boolean mainExit = false;
+        boolean previous = false;
+        int subMenuOption;
+
+        switch (poption) {
+            case 0: // Exits the app.
+                mainExit = true;
+                break;
+            case 1: // Shows the menu for the first option
+                do {
+                    showOpt1Menu();
+                    subMenuOption = readOption();
+                    previous = exeOpt1Action(subMenuOption);
+                } while (!previous);
+                break;
+            case 2: // Shows the menu for the second option
+                do {
+                    showOpt2Menu();
+                    subMenuOption = readOption();
+                    previous = exeOpt2Action(subMenuOption);
+                } while (!previous);
+                break;
+            case 3: // Shows the menu for the third option
+                do {
+                    showOpt3Menu();
+                    subMenuOption = readOption();
+                    previous = exeOpt3Action(subMenuOption);
+                } while (!previous);
+                break;
+            default: // Any value that isn't in the options
+                out.println("Invalid option.");
+                out.println();
+                break;
+        }
+        return mainExit;
+    }
+
+    static boolean exeOpt1Action(int poption) {
+        boolean previous = false;
+
+        switch (poption) {
+            case 0: // Goes back to the previous menu.
+                previous = true;
+                break;
+            case 1: //
+                break;
+            case 2: //
+                break;
+            default: // Any value that isn't in the options
+                out.println("Invalid option.");
+                out.println();
+                break;
+        }
+        return previous;
+    }
+
+    static boolean exeOpt2Action(int poption) {
+        boolean previous = false;
+
+        switch (poption) {
+            case 0: // Goes back to the previous menu.
+                previous = true;
+                break;
+            case 1: //
+                break;
+            case 2: //
+                break;
+            default: // Any value that isn't in the options
+                out.println("Invalid option.");
+                out.println();
+                break;
+        }
+        return previous;
+    }
+
+    static boolean exeOpt3Action(int poption) {
+        boolean previous = false;
+
+        switch (poption) {
+            case 0: // Goes back to the previous menu.
+                previous = true;
+                break;
+            case 1: //
+                break;
+            case 2: //
+                break;
+            default: // Any value that isn't in the options
+                out.println("Invalid option.");
+                out.println();
+                break;
+        }
+        return previous;
+    }
+}
