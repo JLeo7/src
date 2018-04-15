e3e2cf9740a02a411c9adb2a1f905f6faa0312fe
esteban-sancho1 <34180324+esteban-sancho1@users.noreply.github.com>
2018-04-15 00:01:31 -0600

    arreglo SQL telefono cliente

--- com/softwareCelestial/multis/MultiCliente.java
+++ com/softwareCelestial/multis/MultiCliente.java
@@ -58,7 +58,7 @@ public class MultiCliente {
             rs = BD.ejecutarSQL("SELECT id_cliente FROM cliente WHERE cedula_juridica = '" + cedJuridica + "'", true);
             BD.ejecutarSQL("DELETE FROM telefonos_cliente WHERE id_cliente = '"+rs+"'");
             for (String var : telefonos) {
-                BD.ejecutarSQL("INSERT INTO telefonos_cliente(numero_telefonico, '" + rs.getString("id_cliente") + "')");
+                BD.ejecutarSQL("INSERT INTO telefonos_cliente(telefono, id_cliente) VALUES('"+var+"', '"+rs.getString("id_cliente")+"')");
             }
 
         }catch(Exception e){
